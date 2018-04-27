fb0d90f0b1ae516e7683ed280655cf5629b7f6e9
jiangwei <jiangwei07061625@163.com>
2018-04-27 23:43:30 +0800

    add wx shop config table

---
+++ syLibs/Factories/SyTaskMysqlFactory.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-3-10
+ * Time: 21:47
+ */
+namespace Factories;
+
+use Entities\SyTask\TaskBaseEntity;
+use Entities\SyTask\TaskLogEntity;
+use Entities\SyTask\WxshopConfigEntity;
+use Traits\SimpleTrait;
+
+class SyTaskMysqlFactory {
+    use SimpleTrait;
+
+    /**
+     * @return \Entities\SyTask\TaskBaseEntity
+     */
+    public static function TaskBaseEntity(){
+        return new TaskBaseEntity();
+    }
+    /**
+     * @return \Entities\SyTask\TaskLogEntity
+     */
+    public static function TaskLogEntity(){
+        return new TaskLogEntity();
+    }
+    /**
+     * @return \Entities\SyTask\WxshopConfigEntity
+     */
+    public static function WxshopConfigEntity(){
+        return new WxshopConfigEntity();
+    }
+}
