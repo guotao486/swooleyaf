c15dc63193c563583da9823c9303f6d3572cc4ab
jiangwei <jiangwei07061625@163.com>
2018-06-01 12:10:12 +0800

    validator change from frame to project

--- libs_project/Constant/Project.php
+++ libs_project/Constant/Project.php
@@ -25,6 +25,34 @@ final class Project {
     //数据常量
     const DATA_KEY_SESSION_TOKEN = '_sytoken'; //键名-session标识
 
+    //校验器常量
+    const VALIDATOR_STRING_TYPE_REQUIRED = 'string_required'; //字符串类型-必填
+    const VALIDATOR_STRING_TYPE_MIN = 'string_min'; //字符串类型-最小长度
+    const VALIDATOR_STRING_TYPE_MAX = 'string_max'; //字符串类型-最大长度
+    const VALIDATOR_STRING_TYPE_REGEX = 'string_regex'; //字符串类型-正则表达式
+    const VALIDATOR_STRING_TYPE_PHONE = 'string_phone'; //字符串类型-手机号码
+    const VALIDATOR_STRING_TYPE_TEL = 'string_tel'; //字符串类型-联系方式
+    const VALIDATOR_STRING_TYPE_EMAIL = 'string_email'; //字符串类型-邮箱
+    const VALIDATOR_STRING_TYPE_URL = 'string_url'; //字符串类型-URL链接
+    const VALIDATOR_STRING_TYPE_JSON = 'string_json'; //字符串类型-JSON
+    const VALIDATOR_STRING_TYPE_SIGN = 'string_sign'; //字符串类型-请求签名
+    const VALIDATOR_STRING_TYPE_BASE_IMAGE = 'string_baseimage'; //字符串类型-base64编码图片
+    const VALIDATOR_STRING_TYPE_IP = 'string_ip'; //字符串类型-IP
+    const VALIDATOR_STRING_TYPE_LNG = 'string_lng'; //字符串类型-经度
+    const VALIDATOR_STRING_TYPE_LAT = 'string_lat'; //字符串类型-纬度
+    const VALIDATOR_STRING_TYPE_NO_JS = 'string_nojs'; //字符串类型-不允许js脚本
+    const VALIDATOR_STRING_TYPE_NO_EMOJI = 'string_noemoji'; //字符串类型-不允许emoji表情
+    const VALIDATOR_STRING_TYPE_ZH = 'string_zh'; //字符串类型-中文,数字,字母
+    const VALIDATOR_INT_TYPE_REQUIRED = 'int_required'; //整数类型-必填
+    const VALIDATOR_INT_TYPE_MIN = 'int_min'; //整数类型-最小值
+    const VALIDATOR_INT_TYPE_MAX = 'int_max'; //整数类型-最大值
+    const VALIDATOR_INT_TYPE_IN = 'int_in'; //整数类型-取值枚举
+    const VALIDATOR_INT_TYPE_BETWEEN = 'int_between'; //整数类型-取值区间
+    const VALIDATOR_DOUBLE_TYPE_REQUIRED = 'double_required'; //浮点数类型-必填
+    const VALIDATOR_DOUBLE_TYPE_MIN = 'double_min'; //浮点数类型-最小值
+    const VALIDATOR_DOUBLE_TYPE_MAX = 'double_max'; //浮点数类型-最大值
+    const VALIDATOR_DOUBLE_TYPE_BETWEEN = 'double_between'; //浮点数类型-取值区间
+
     //公共常量
     const COMMON_PAGE_DEFAULT = 1; //默认页数
     const COMMON_LIMIT_DEFAULT = 10; //默认分页限制
