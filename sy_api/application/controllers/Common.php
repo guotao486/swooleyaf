05b22e7d46fdbcdcba67b53020e3ada28a9fcd0e
jiangwei <jiangwei07061625@163.com>
2018-07-17 08:58:36 +0800

    update sy server cookie domain

--- sy_api/application/controllers/Common.php
+++ sy_api/application/controllers/Common.php
@@ -15,16 +15,6 @@ class CommonController extends \SyFrame\BaseController {
         $token = \Tool\SySession::getSessionId();
         $_COOKIE[\Constant\Project::DATA_KEY_SESSION_TOKEN] = $token;
         $expireTime = \Tool\Tool::getNowTime() + 604800;
-        $totalDomain = \Tool\Tool::getConfig('project.' . SY_ENV . SY_PROJECT . '.domain.cookie');
-        if(isset($_SERVER['SY-DOMAIN'])){
-            $domain = trim($_SERVER['SY-DOMAIN']);
-            if(!in_array($domain, $totalDomain)){
-                throw new \Exception\Common\CheckException('请求域名不支持', \Constant\ErrorCode::COMMON_SERVER_ERROR);
-            }
-        } else {
-            $domain = $totalDomain[0];
-        }
-
-        \Response\SyResponseHttp::cookie(\Constant\Project::DATA_KEY_SESSION_TOKEN, $token, $expireTime, '/', $domain);
+        \Response\SyResponseHttp::cookie(\Constant\Project::DATA_KEY_SESSION_TOKEN, $token, $expireTime, '/', $_SERVER['SY-DOMAIN']);
     }
 }
