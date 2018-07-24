31c2f62e9071e052770f41d7fd7e2f736605d776
jiangwei <jiangwei07061625@163.com>
2018-07-24 11:35:43 +0800

    test kafka

--- helper_messagequeue.php
+++ helper_messagequeue.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/helper_load.php';
 
 define('SY_MODULE', SY_PROJECT . 'task');
+define('SY_SERVER_IP', (string)\Tool\Tool::getConfig('syserver.base.server.host'));
 set_exception_handler('\SyError\ErrorHandler::handleException');
 set_error_handler('\SyError\ErrorHandler::handleError');
 \Log\Log::setPath(SY_LOG_PATH);
