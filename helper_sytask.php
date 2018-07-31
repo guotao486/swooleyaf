0827970f5e40c6f78fb7a3a5a2990abee456a137
jiangwei <jiangwei07061625@163.com>
2018-07-31 14:29:41 +0800

    move db check to sytask

--- helper_sytask.php
+++ helper_sytask.php
@@ -1,26 +1,31 @@
 <?php
 require_once __DIR__ . '/helper_load.php';
 
-function sendSyGetReq(string $url) {
-    \Tool\Tool::sendCurlReq([
-        CURLOPT_URL => $url,
-        CURLOPT_TIMEOUT_MS => 2000,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_HEADER => false,
-        CURLOPT_RETURNTRANSFER => true,
-    ]);
-}
-
 $projects = include(__DIR__ . '/helper_projects.php');
-$container = new \SyTask\SyModuleTaskContainer();
-
 $timeArr = explode('-', date('H-i'));
 $minute = (int)$timeArr[1];
 $hour = (int)$timeArr[0];
 
-$needMinute1 = $minute % 5;
+//定时检测数据库连接
+if(in_array($minute, [0, 30])){
+    $syPack = new \Tool\SyPack();
+    $syPack->setCommandAndData(\Tool\SyPack::COMMAND_TYPE_RPC_CLIENT_SEND_API_REQ, [
+        'api_uri' => '/Index/Index/check',
+        'api_params' => [],
+    ]);
+    $checkContent = $syPack->packData();
 
+    foreach ($projects as $eProject) {
+        if($eProject['module_type'] == 'rpc'){
+            foreach ($eProject['listens'] as $eListen) {
+                \Tool\Tool::sendSyRpcReq($eListen['host'], $eListen['port'], $checkContent);
+            }
+        }
+    }
+}
+
+$container = new \SyTask\SyModuleTaskContainer();
+$needMinute1 = $minute % 5;
 $taskParams = [
     'task_minute' => $minute,
     'task_hour' => $hour,
