ac09d4c8bc59ccfb28e1722e273c39887d560ae9
jiangwei <jiangwei07061625@163.com>
2018-11-01 11:13:46 +0800

    ali add auth and life

---
+++ libs_frame/Ali/Auth/AuthToken.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/11/1 0001
+ * Time: 10:13
+ */
+namespace Ali\Auth;
+
+use Ali\AliBase;
+use Constant\ErrorCode;
+use Exception\Ali\AliPayException;
+
+class AuthToken extends AliBase {
+    /**
+     * 准许类型
+     * @var string
+     */
+    private $grant_type = '';
+    /**
+     * 授权码
+     * @var string
+     */
+    private $code = '';
+    /**
+     * 刷新令牌
+     * @var string
+     */
+    private $refresh_token = '';
+
+    public function __construct(string $appId){
+        parent::__construct($appId);
+        $this->setMethod('alipay.open.auth.token.app');
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $grantType
+     * @throws \Exception\Ali\AliPayException
+     */
+    public function setGrantType(string $grantType){
+        if(in_array($grantType, ['authorization_code', 'refresh_token',])){
+            $this->biz_content['grant_type'] = $grantType;
+        } else {
+            throw new AliPayException('准许类型不合法', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $code
+     * @throws \Exception\Ali\AliPayException
+     */
+    public function setCode(string $code){
+        if(ctype_alnum($code) && (strlen($code) <= 40)){
+            $this->biz_content['code'] = $code;
+            unset($this->biz_content['refresh_token']);
+        } else {
+            throw new AliPayException('授权码不合法', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+    }
+
+    /**
+     * @param string $refreshToken
+     * @throws \Exception\Ali\AliPayException
+     */
+    public function setRefreshToken(string $refreshToken){
+        if(ctype_alnum($refreshToken) && (strlen($refreshToken) <= 40)){
+            $this->biz_content['refresh_token'] = $refreshToken;
+            unset($this->biz_content['code']);
+        } else {
+            throw new AliPayException('刷新令牌不合法', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if (!isset($this->biz_content['grant_type'])) {
+            throw new AliPayException('准许类型不能为空', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+        if(($this->biz_content['grant_type'] == 'authorization_code') && !isset($this->biz_content['code'])){
+            throw new AliPayException('授权码不能为空', ErrorCode::ALIPAY_PARAM_ERROR);
+        } else if(($this->biz_content['grant_type'] == 'refresh_token') && !isset($this->biz_content['refresh_token'])){
+            throw new AliPayException('刷新令牌不能为空', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+
+        return $this->getContent();
+    }
+}
---
+++ libs_frame/Ali/Life/MessageGet.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 姜伟
+ * Date: 2018/11/1 0001
+ * Time: 10:55
+ */
+namespace Ali\Life;
+
+use Ali\AliBase;
+use Constant\ErrorCode;
+use Exception\Ali\AliPayException;
+
+class MessageGet extends AliBase {
+    /**
+     * 模板ID
+     * @var string
+     */
+    private $template_id = '';
+
+    public function __construct(string $appId){
+        parent::__construct($appId);
+        $this->setMethod('alipay.open.public.template.message.get');
+    }
+
+    private function __clone(){
+    }
+
+    /**
+     * @param string $templateId
+     * @throws \Exception\Ali\AliPayException
+     */
+    public function setTemplateId(string $templateId){
+        if(ctype_alnum($templateId) && (strlen($templateId) <= 20)){
+            $this->biz_content['template_id'] = $templateId;
+        } else {
+            throw new AliPayException('模板ID不合法', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+    }
+
+    public function getDetail() : array {
+        if (!isset($this->biz_content['template_id'])) {
+            throw new AliPayException('模板ID不能为空', ErrorCode::ALIPAY_PARAM_ERROR);
+        }
+
+        return $this->getContent();
+    }
+}
