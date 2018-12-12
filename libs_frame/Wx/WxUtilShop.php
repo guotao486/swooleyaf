b6a383b1a351bb43bb11301fbe65d88005862df2
jiangwei <jiangwei07061625@163.com>
2018-12-12 18:12:46 +0800

    wx add company pay bank

--- libs_frame/Wx/WxUtilShop.php
+++ libs_frame/Wx/WxUtilShop.php
@@ -9,8 +9,10 @@ namespace Wx;
 
 use Constant\ErrorCode;
 use DesignPatterns\Singletons\WxConfigSingleton;
+use Exception\Wx\WxException;
 use Tool\Tool;
 use Traits\SimpleTrait;
+use Wx\Shop\PayCompanyBankPublicKey;
 
 final class WxUtilShop extends WxUtilBaseAlone {
     use SimpleTrait;
@@ -103,5 +105,50 @@ final class WxUtilShop extends WxUtilBaseAlone {
         }
 
         return $resArr;
+    }
+
+    /**
+     * 获取企业银行付款公钥
+     * @param string $appId
+     * @return string
+     * @throws \Exception\Wx\WxException
+     */
+    public static function getCompanyBankPayPublicKey(string $appId) : string {
+        $shopConfig = WxConfigSingleton::getInstance()->getShopConfig($appId);
+        $fileName = Tool::getConfig('project.' . SY_ENV . SY_PROJECT . '.dir.store.resources') . '/certs/wxcompanybank_' . $shopConfig->getPayMchId() . '.pem';
+        if(!file_exists($fileName)){
+            $bankPublicKey = new PayCompanyBankPublicKey($appId);
+            $detail = $bankPublicKey->getDetail();
+            unset($bankPublicKey);
+            if($detail['code'] > 0){
+                throw new WxException($detail['message'], $detail['code']);
+            }
+            if(!file_put_contents($fileName, $detail['data']['pub_key'])){
+                throw new WxException('写入银行公钥文件失败', ErrorCode::WX_POST_ERROR);
+            }
+
+            $output = [];
+            $commandStatus = 0;
+            $command = 'openssl rsa -RSAPublicKey_in -in ' . $fileName . ' -out ' . $fileName;
+            exec($command, $output, $commandStatus);
+            if($commandStatus == -1){
+                throw new WxException('生成银行公钥文件失败', ErrorCode::WX_POST_ERROR);
+            }
+        }
+
+        return openssl_pkey_get_public(file_get_contents($fileName));
+    }
+
+    /**
+     * 企业银行付款rsa加密
+     * @param string $publicKey
+     * @param string $data
+     * @return string
+     */
+    public static function encryptRsaCompanyBank(string $publicKey,string $data) : string {
+        $encryptData = '';
+        openssl_public_encrypt($data, $encryptData, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
+
+        return base64_encode($encryptData);
     }
 }
