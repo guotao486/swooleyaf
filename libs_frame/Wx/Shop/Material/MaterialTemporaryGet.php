dcca347e57302446c86ec33496a7ea9675eb27d4
jiangwei <jiangwei07061625@163.com>
2018-12-13 14:24:44 +0800

    wx add shop material

--- libs_frame/Wx/Shop/Material/MaterialTemporaryGet.php
+++ libs_frame/Wx/Shop/Material/MaterialTemporaryGet.php
@@ -2,37 +2,46 @@
 /**
  * Created by PhpStorm.
  * User: 姜伟
- * Date: 2018/9/12 0012
- * Time: 8:38
+ * Date: 2018/12/13 0013
+ * Time: 9:37
  */
-namespace Wx\Shop;
+namespace Wx\Shop\Material;
 
 use Constant\ErrorCode;
 use Exception\Wx\WxException;
 use Tool\Tool;
 use Wx\WxBaseShop;
 use Wx\WxUtilBase;
-use Wx\WxUtilBaseAlone;
+use Wx\WxUtilShop;
 
-class DownloadMedia extends WxBaseShop {
+class MaterialTemporaryGet extends WxBaseShop {
+    /**
+     * 公众号ID
+     * @var string
+     */
+    private $appid = '';
     /**
      * 输出目录
      * @var string
      */
     private $output_dir = '';
     /**
-     * 媒体ID
+     * 媒体文件ID
      * @var string
      */
     private $media_id = '';
 
-    public function __construct(string $appId){
+    public function __construct(string $appId,string $protocol='https'){
         parent::__construct();
-        $this->serviceUrl = 'http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=';
-        $this->reqData['appid'] = $appId;
+        if(!in_array($protocol, ['http', 'https'])){
+            throw new WxException('协议不支持', ErrorCode::WX_PARAM_ERROR);
+        }
+
+        $this->serviceUrl = $protocol . '://api.weixin.qq.com/cgi-bin/media/get?access_token=';
+        $this->appid = $appId;
     }
 
-    public function __clone(){
+    private function __clone(){
     }
 
     /**
@@ -55,7 +64,7 @@ class DownloadMedia extends WxBaseShop {
         if(strlen($mediaId) > 0){
             $this->reqData['media_id'] = $mediaId;
         } else {
-            throw new WxException('媒体ID不合法', ErrorCode::WX_PARAM_ERROR);
+            throw new WxException('媒体文件ID不合法', ErrorCode::WX_PARAM_ERROR);
         }
     }
 
@@ -64,26 +73,28 @@ class DownloadMedia extends WxBaseShop {
             throw new WxException('输出目录不能为空', ErrorCode::WX_PARAM_ERROR);
         }
         if(!isset($this->reqData['media_id'])){
-            throw new WxException('媒体ID不能为空', ErrorCode::WX_PARAM_ERROR);
+            throw new WxException('媒体文件ID不能为空', ErrorCode::WX_PARAM_ERROR);
         }
 
         $resArr = [
             'code' => 0,
         ];
 
-        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . WxUtilBaseAlone::getAccessToken($this->reqData['appid']) . '&media_id=' . $this->reqData['media_id'];
+        $this->curlConfigs[CURLOPT_URL] = $this->serviceUrl . WxUtilShop::getAccessToken($this->appid) . '&media_id=' . $this->reqData['media_id'];
         $this->curlConfigs[CURLOPT_TIMEOUT_MS] = 3000;
         $sendRes = WxUtilBase::sendGetReq($this->curlConfigs);
         $sendData = Tool::jsonDecode($sendRes);
-        if(is_array($sendData)){
+        if(!is_array($sendData)){
+            $fileName = $this->reqData['output_dir'] . $this->reqData['media_id'];
+            file_put_contents($fileName, $sendRes);
+            $resArr['data'] = [
+                'media_path' => $fileName,
+            ];
+        } else if(isset($sendData['errcode'])){
             $resArr['code'] = ErrorCode::WX_GET_ERROR;
             $resArr['message'] = $sendData['errmsg'];
         } else {
-            $fileName = $this->reqData['output_dir'] . $this->reqData['media_id'] . '.jpg';
-            file_put_contents($fileName, $sendRes);
-            $resArr['data'] = [
-                'image_path' => $fileName,
-            ];
+            $resArr['data'] = $sendData;
         }
 
         return $resArr;
--- libs_frame/Wx/WxBaseShop.php
+++ libs_frame/Wx/WxBaseShop.php
@@ -8,6 +8,18 @@
 namespace Wx;
 
 abstract class WxBaseShop extends WxBase {
+    const MATERIAL_TYPE_IMAGE = 'image';
+    const MATERIAL_TYPE_VOICE = 'voice';
+    const MATERIAL_TYPE_VIDEO = 'video';
+    const MATERIAL_TYPE_THUMB = 'thumb';
+
+    protected static $totalMaterialType = [
+        self::MATERIAL_TYPE_IMAGE => '图片',
+        self::MATERIAL_TYPE_VOICE => '语音',
+        self::MATERIAL_TYPE_VIDEO => '视频',
+        self::MATERIAL_TYPE_THUMB => '缩略图',
+    ];
+
     public function __construct(){
         parent::__construct();
     }
